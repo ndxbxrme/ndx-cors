0dcbc223b82f83dc9bdf9b2e8ee5875b5131b22a
ndxbxrme <lewis_the_cat@hotmail.com>
2017-02-23 10:08:28 +0000

    auth route

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   module.exports = function(ndx) {
-    return ndx.app.use('(api|auth)', function(req, res, next) {
+    return ndx.app.use('/:var(api|auth)?', function(req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
