7533bcf97c4afc4dc0a2098b5ef75853c81433a0
ndxbxrme <lewis_the_cat@hotmail.com>
2018-09-20 14:55:27 +0100

    -ndxhost

--- build/index.js
+++ build/index.js
@@ -3,7 +3,7 @@
   module.exports = function(ndx) {
     return ndx.app.use('/:var(api|auth)?', function(req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, ndxhost');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
       if (req.method === 'OPTIONS') {
         return res.end();
--- src/index.coffee
+++ src/index.coffee
@@ -3,7 +3,7 @@
 module.exports = (ndx) ->
   ndx.app.use '/:var(api|auth)?', (req, res, next) ->
     res.setHeader 'Access-Control-Allow-Origin', '*'
-    res.setHeader 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, ndxhost'
+    res.setHeader 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     res.setHeader 'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'
     if req.method is 'OPTIONS'
       res.end()
